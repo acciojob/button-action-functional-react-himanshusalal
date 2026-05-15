3de6148a622116ef1adf85dedfa4004ea6d781e6
himanshusalal <himanshusalal517@gmail.com>
2026-05-15 13:40:54 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div>
+      <button id="click" onClick={() => setShowParagraph(true)}>
+        Show Paragraph
+      </button>
+
+      {showParagraph && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy.
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
